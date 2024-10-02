f945705e00c3d196b08183f88c6ce0860893667f
muskovszky.tamas <muskovszky.tamas@BURG-1E05-03>
2024-10-02 12:29:41 +0200

    asdf

--- desserts-frontend/src/components/Cart.tsx
+++ desserts-frontend/src/components/Cart.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import CartItem from "./CartItem";
 import { CartContext } from "../pages/Home";
 
@@ -7,12 +7,41 @@ export type CartItemType = {
   price: number;
 };
 
+export type VisibleCartType = {
+  item: CartItemType;
+  quantity: number;
+};
+
 const Cart = () => {
   const cartCtx = useContext(CartContext);
   if (!cartCtx) {
     throw new Error("nincs kontext");
   }
-  const {cart} = cartCtx;
+  const { cart } = cartCtx;
+
+  const [visibleCart, setVisibleCart] = useState<VisibleCartType[]>([]);
+
+  useEffect(() => {
+    const unique: string[] = [];
+    cart.forEach((i) => {
+      if (!unique.includes(i.name)) {
+        unique.push(i.name);
+      }
+    });
+
+    let tempCart = [];
+
+    unique.forEach((d) => {
+      let counter = 0;
+      cart.forEach((c) => {
+        if (c.name === d) {
+          counter++;
+        }
+      });
+    //   let visibleItem: VisibleCartType = { item: c }; TODO!
+    });
+  }, [cartCtx]);
+
   return (
     <aside className="cart">
       <h2>Your Cart ({cart.length})</h2>
